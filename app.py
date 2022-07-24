5c8c054af9103bfa03f82e51dc217a2daf7e2bbf
Poojithagit <108671477+Poojithagit@users.noreply.github.com>
2022-07-24 22:07:28 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 import streamlit as st
-string = "Odd-Even Finder"
-st.set_page_config(page_title=string, page_icon="🔍")
 
-st.title('Odd-Even Finder')
-x = st.number_input('Enter a number')
-if (x).is_integer():
-    if x%2==0:st.write("your number is even")
-    elif x%2!=0:st.write("your number is odd")
-else:st.write("please enter integer number")
+string = "Multiplier"
+st.set_page_config(page_title=string)
+
+st.write("""
+# Two Numbler Multiplication App
+This app multiplies two number
+""")
+
+
+st.header('User Input Parameters')
+
+def user_input_features():
+    n1 = st.number_input("Enter Number-1")
+    n2 = st.number_input("Enter Number-2")
+
+    return n1, n2
+
+n1, n2 = user_input_features()
+
+if st.button('Calculate'):
+    st.write(n1-n2)
